b0df5367e549f0edcf6cc1531ca33a7b6bb78592
追梦小窝 <mail@54zm.com>
2020-04-29 21:27:36 +0800

    1

--- src/Address.php
+++ src/Address.php
@@ -28,4 +28,27 @@ class Address
             $ip = "Unknown";
         return $ip;
     }
+
+    /**
+     * 根据ip获取用户的地址
+     * @param $ip
+     * @return array|bool
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/29 21:27
+     */
+    public static function address($ip)
+    {
+        if ("127.0.0.1" == $ip) {
+            return ['origip' => $ip, 'location' => '本地开发'];
+        }
+        $url      = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={$ip}&resource_id=6006";
+        $response = HttpCurl::doGet($url);
+        $response = mb_convert_encoding($response, 'utf-8', 'GB2312');
+        $re       = json_decode($response, true);
+        if (empty($re['data'])) {
+            return false;
+        } else {
+            return $re['data']['0'];
+        }
+    }
 }
---
+++ src/HttpCurl.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Iszmxw\IpAddress;
+
+
+class HttpCurl
+{
+    /**
+     * get请求
+     * @param $url
+     * @return bool|string
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/29 21:25
+     */
+    public static function doGet($url)
+    {
+        //初始化
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // 执行后不直接打印出来
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        // 跳过证书检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // 不从证书中检查SSL加密算法是否存在
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+        //执行并获取HTML文档内容
+        $output = curl_exec($ch);
+
+        //释放curl句柄
+        curl_close($ch);
+
+        return $output;
+    }
+
+    /**
+     * post请求
+     * @param $url
+     * @param $post_data
+     * @param string $header
+     * @return bool|string
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/29 21:25
+     */
+    public static function doPost($url, $post_data, $header = '')
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // 执行后不直接打印出来
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // 设置请求方式为post
+        curl_setopt($ch, CURLOPT_POST, true);
+        // post的变量
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        // 请求头，可以传数组
+        curl_setopt($ch, CURLOPT_HEADER, $header);
+        // 跳过证书检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // 不从证书中检查SSL加密算法是否存在
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+}

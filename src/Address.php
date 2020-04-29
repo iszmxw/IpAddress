7a1cf3123df0bf6008f873c444a716dbf1e48d94
追梦小窝 <mail@54zm.com>
2020-04-29 18:05:53 +0800

    初始化仓库

---
+++ src/Address.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iszmxw
+ * Date: 2020/04/29
+ * Time: 17:51
+ */
+
+namespace Iszmxw\IpAddress;
+
+class Address
+{
+    public function ip()
+    {
+        if (@$_SERVER["HTTP_X_FORWARDED_FOR"])
+            $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        else if (@$_SERVER["HTTP_CLIENT_IP"])
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        else if (@$_SERVER["REMOTE_ADDR"])
+            $ip = $_SERVER["REMOTE_ADDR"];
+        else if (@getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if (@getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if (@getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else
+            $ip = "Unknown";
+        return $ip;
+    }
+}
